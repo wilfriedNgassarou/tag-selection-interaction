5a39b8a0ceae936d5304296ddfb6d79f9244bbce
unknown <wilfriedngassarou23@gmail.com>
2025-02-25 20:51:23 +0100

    Added constants

---
+++ src/constants/tags.ts
@@ -0,0 +1,21 @@
+export type Tag = typeof tags[number]
+
+const tags = [
+  { name: 'node' },
+  { name: 'javascript' },
+  { name: 'express' },
+  { name: 'react' },
+  { name: 'vue' },
+  { name: 'next' },
+  { name: 'typescript' },
+  { name: 'svelte' },
+  { name: 'gatsby' },
+  { name: 'knockout' },
+  { name: 'backbone' },
+  { name: 'ember' },
+  { name: 'chai' },
+  { name: 'mocha' },
+  { name: 'jest' },
+]
+
+export { tags }
